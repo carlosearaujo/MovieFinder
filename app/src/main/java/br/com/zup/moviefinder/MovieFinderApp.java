08fc63200ab7208e6b71b3aa4ba35faea51ce374
carlosearaujo <carlos.e.a.eti@gmail.com>
2016-07-23 19:10:25 +0000

    Basic structure

---
+++ app/src/main/java/br/com/zup/moviefinder/MovieFinderApp.java
@@ -0,0 +1,9 @@
+package br.com.zup.moviefinder;
+
+import br.com.thindroid.commons.Application;
+
+/**
+ * Created by Carlos on 23/07/2016.
+ */
+public class MovieFinderApp extends Application {
+}
